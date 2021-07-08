70a95518bf45d5dc2e58763bb92fc01e8267d59a
Pertoldi <antoine.pertoldi@gmail.com>
2021-07-08 16:35:02 +0200

    fix coDeco

--- JS/header.js
+++ JS/header.js
@@ -1,20 +1,24 @@
 function main() {
-    if (localStorage.getItem('token') == null) {
-        console.log("la personne n'est pas identifiéé !");
-    } else {
-        console.log("la personne est identifiée !");
-        const coDeco = document.getElementById("dynamicLabel");
-        if (coDeco == null) {
-            console.log('coDeco===null header.js ligne.8 !');
-        }else {
-            coDeco.setAttribute("href", "./index.html");
-            coDeco.innerHTML = "Deconnexion";
-            coDeco.addEventListener('click', () => {
-                localStorage.removeItem('token');
-                localStorage.removeItem('userId');
-                alert('Vous etes deconnecter !');
-            });
+    document.addEventListener('DOMContentLoaded', (event) => {
+        if (localStorage.getItem('token') == null) {
+            console.log("la personne n'est pas identifiéé !");
+        } else {
+            console.log("la personne est identifiée !");
+            const coDeco = document.getElementById("dynamicLabel");
+            if (coDeco == null) {
+                console.log('coDeco===null header.js ligne.8 !');
+            }else {
+                console.log('coDeco!=null header.js ligne.8 !');
+                coDeco.setAttribute("href", "./index.html");
+                coDeco.innerHTML = "Deconnexion";
+                coDeco.addEventListener('click', () => {
+                    localStorage.removeItem('token');
+                    localStorage.removeItem('userId');
+                    alert('Vous etes deconnecter !');
+                });
+            }
         }
-    }
+    });
+
 }
 main();
