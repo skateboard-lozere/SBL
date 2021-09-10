73be07b78c93a9616310bbafd831704c180dd3f9
Pertoldi <antoine.pertoldi@gmail.com>
2021-09-10 14:42:03 +0200

    Skate Game Fixed

--- JS/game.js
+++ JS/game.js
@@ -11,16 +11,16 @@ let score = 0;
 const spriteStart = new Image();
 spriteStart.src = "./images/minigame/start.png";
 const start = {
-     sX : 40,
-     sY : 55,
-     destinationX : canvas.width/2,
-     destinationY : canvas.height/2,
-     W : 165,
-     H :  65,
+    sX: 40,
+    sY: 55,
+    destinationX: canvas.width / 2,
+    destinationY: canvas.height / 2,
+    W: 165,
+    H: 65,
 
-    draw : function() {
+    draw: function () {
         if (state.current == state.start) {
-            context.drawImage(spriteStart,this.sX, this.sY, this.W, this.H, this.destinationX - this.W/2, this.destinationY - this.H/2, this.W, this.H);
+            context.drawImage(spriteStart, this.sX, this.sY, this.W, this.H, this.destinationX - this.W / 2, this.destinationY - this.H / 2, this.W, this.H);
         }
     }
 }
@@ -29,7 +29,7 @@ const start = {
 const plateau = {
     x: 0,
     y: 200,
-    draw : function() {
+    draw: function () {
         context.fillStyle = "#000000";
         context.fillRect(this.x, this.y, canvas.width, 100); //coord x et y puis largeur hauteur
     },
@@ -41,13 +41,13 @@ class Obstacle {
     constructor() {
         this.x = canvas.width;
         this.y = 180,
-        this.width = 20,
-        this.height = 20
+            this.width = 20,
+            this.height = 20
     }
     draw() {
         if (this.x > -1) {
-        context.fillStyle = "#000000";
-        context.fillRect(this.x, this.y, this.width, this.height);
+            context.fillStyle = "#000000";
+            context.fillRect(this.x, this.y, this.width, this.height);
         } else {
             this.x = canvas.width + Math.floor(Math.random() * Math.floor(300));
             console.log(`création de l'obstacle à ${this.x}px du bords`);
@@ -57,25 +57,26 @@ class Obstacle {
         if (state.current == state.game) {
             this.x = this.x - 3;
             //quand le skateur touche l'obstacle et qu'il n'a pas une frame de saut
-            if (skateur.destinationX == this.x || skateur.destinationX == (this.x + 1)) {
+            if (skateur.destinationX == this.x || skateur.destinationX == (this.x + 1) || skateur.destinationX == (this.x + 2)) {
                 console.log('OBSTACLE !');
                 console.log('Frame du skateur :', skateur.frame);
-                   if( skateur.frame <= 5 || skateur.frame >= 12) {
-                       if (skateur.frame >= 9) {
+                if (skateur.frame < 5 || skateur.frame >= 12) {
+                    if (skateur.frame >= 12) {
                         setTimeout(() => {
-                        state.current = state.over;
-                        alert(`Votre score: ${score}`);
-                        score = 0;
-                        }, 150);
+                            state.current = state.over;
+                            alert(`Votre score: ${score}`);
+                            score = 0;
+                        }, 100);
                     } else {
                         setTimeout(() => {
                             state.current = state.over;
                             alert(`Votre score: ${score}`);
                             score = 0;
-                            }, 100);
+                        }, 100);
                     }
                 } else {
                     score += 1;
+                    console.log('score is :', score)
                 }
             }
         }
@@ -84,37 +85,37 @@ class Obstacle {
 
 var obstacle = new Obstacle;
 var obstacle2 = new Obstacle;
-obstacle2.x = canvas.width + canvas.width/2;
+obstacle2.x = canvas.width + canvas.width / 2;
 
 //MENU GAME OVER
 const spriteOver = new Image();
 spriteOver.src = "./images/minigame/over.png";
 const gameOver = {
-     sX : 40,
-     sY : 60,
-     destinationX : canvas.width/2,
-     destinationY : canvas.height/2,
-     W : 300,
-     H : 162,
+    sX: 40,
+    sY: 60,
+    destinationX: canvas.width / 2,
+    destinationY: canvas.height / 2,
+    W: 300,
+    H: 162,
 
-    draw : function() {
-        if (state.current == state.over){
-            context.drawImage(spriteOver,this.sX, this.sY, 400, 215, this.destinationX - this.W/2, this.destinationY - this.H/2, this.W, this.H);
-        } 
+    draw: function () {
+        if (state.current == state.over) {
+            context.drawImage(spriteOver, this.sX, this.sY, 400, 215, this.destinationX - this.W / 2, this.destinationY - this.H / 2, this.W, this.H);
+        }
     }
 }
 
 //GAME STATE
 const state = {
-    current : 1,
-    start : 1,
-    game : 2,
-    over : 3
+    current: 1,
+    start: 1,
+    game: 2,
+    over: 3
 }
 
 //CONTROL THE GAME
-canvas.addEventListener('click', function(evt){
-    switch(state.current){
+canvas.addEventListener('click', function (evt) {
+    switch (state.current) {
         case state.start:
             state.current = state.game;
             break;
@@ -131,52 +132,52 @@ const spriteSkateur = new Image();
 spriteSkateur.src = "./images/minigame/sprite_skateur.png";
 
 const skateur = {
-    animation : [  
-        {sX: 0, sY: 0}, //Première image on change la source du sprite à chaque élém  -   this.animation[0] etc
-        {sX: 128, sY: 0}, //deuxième image
-        {sX: 2*128, sY: 0}, //troisième image
-        {sX: 3*128, sY: 0},
-        {sX: 4*128, sY: 0},
-        {sX: 5*128, sY: 0},
-        {sX: 6*128, sY: 0},
-        {sX: 7*128, sY: 0},
-        {sX: 8*128, sY: 0},
-        {sX: 9*128, sY: 0},
-        {sX: 10*128, sY: 0},
-        {sX: 11*128, sY: 0},
-        {sX: 12*128, sY: 0},
-        {sX: 13*128, sY: 0},
-        {sX: 14*128, sY: 0},
-        {sX: 15*128, sY: 0},
-        {sX: 16*128, sY: 0},
-        {sX: 17*128, sY: 0},
-        {sX: 18*128, sY: 0},
-        {sX: 19*128, sY: 0},
-        {sX: 20*128, sY: 0},
-        {sX: 21*128, sY: 0}
+    animation: [
+        { sX: 0, sY: 0 }, //Première image on change la source du sprite à chaque élém  -   this.animation[0] etc
+        { sX: 128, sY: 0 }, //deuxième image
+        { sX: 2 * 128, sY: 0 }, //troisième image
+        { sX: 3 * 128, sY: 0 },
+        { sX: 4 * 128, sY: 0 },
+        { sX: 5 * 128, sY: 0 },
+        { sX: 6 * 128, sY: 0 },
+        { sX: 7 * 128, sY: 0 },
+        { sX: 8 * 128, sY: 0 },
+        { sX: 9 * 128, sY: 0 },
+        { sX: 10 * 128, sY: 0 },
+        { sX: 11 * 128, sY: 0 },
+        { sX: 12 * 128, sY: 0 },
+        { sX: 13 * 128, sY: 0 },
+        { sX: 14 * 128, sY: 0 },
+        { sX: 15 * 128, sY: 0 },
+        { sX: 16 * 128, sY: 0 },
+        { sX: 17 * 128, sY: 0 },
+        { sX: 18 * 128, sY: 0 },
+        { sX: 19 * 128, sY: 0 },
+        { sX: 20 * 128, sY: 0 },
+        { sX: 21 * 128, sY: 0 }
     ],
-    destinationX : 200,
-    destinationY : 200,
-    W : 128,
-    H :  213,
-    frame : 0,
-    Ijump : false,
-    counter : 0,
+    destinationX: 200,
+    destinationY: 200,
+    W: 128,
+    H: 213,
+    frame: 0,
+    Ijump: false,
+    counter: 0,
 
-    draw : function() {
+    draw: function () {
         var skate = this.animation[this.frame];
-        context.drawImage(spriteSkateur, skate.sX, skate.sY, this.W, this.H,  this.destinationX - this.W/2, this.destinationY - this.H/2, this.W/2, this.H/2);
+        context.drawImage(spriteSkateur, skate.sX, skate.sY, this.W, this.H, this.destinationX - this.W / 2, this.destinationY - this.H / 2, this.W / 2, this.H / 2);
     },
 
-    update : function() {// fait l'animation
+    update: function () {// fait l'animation
         if (this.Ijump == true) {
             this.period = 6;
-            if (state.current == state.game && frames%this.period == 0) {
-                    this.frame += 1;                    
+            if (state.current == state.game && frames % this.period == 0) {
+                this.frame += 1;
             }
             this.counter += 1;
         }
-        this.frame = this.frame%this.animation.length;
+        this.frame = this.frame % this.animation.length;
         if (this.counter == (this.period * this.animation.length)) {//Pour calculer quand l'animation  est fini
             this.counter = 0;
             this.Ijump = false;
@@ -184,7 +185,7 @@ const skateur = {
         }
     },
 
-    jump : function() {
+    jump: function () {
         this.Ijump = true;
     }
 }
@@ -209,7 +210,7 @@ function update() {
 }
 
 // LOOP
-function loop(){
+function loop() {
     update();
     draw();
     frames++;
--- JS/header.js
+++ JS/header.js
@@ -1,23 +1,25 @@
 function main() {
     document.addEventListener('DOMContentLoaded', (event) => {
-        if (localStorage.getItem('token') == null) {
-            console.log("la personne n'est pas identifiéé !");
-        } else {
-            console.log("la personne est identifiée !");
-            const coDeco = document.getElementById("dynamicLabel");
-            if (coDeco == null) {
-                console.log('coDeco===null header.js ligne.8 !');
-            }else {
-                console.log('coDeco VAUT SOMETING');
-                coDeco.setAttribute("href", "./index.html");
-                coDeco.innerHTML = "Deconnexion";
-                coDeco.addEventListener('click', () => {
-                    localStorage.removeItem('token');
-                    localStorage.removeItem('userId');
-                    alert('Vous etes deconnecter !');
-                });
+        setTimeout(() => {
+            if (localStorage.getItem('token') == null) {
+                console.log("la personne n'est pas identifiéé !");
+            } else {
+                console.log("la personne est identifiée !");
+                const coDeco = document.getElementById("dynamicLabel");
+                if (coDeco == null) {
+                    console.log('coDeco===null header.js ligne.8 !');
+                } else {
+                    console.log('coDeco VAUT SOMETING');
+                    coDeco.setAttribute("href", "./index.html");
+                    coDeco.innerHTML = "Deconnexion";
+                    coDeco.addEventListener('click', () => {
+                        localStorage.removeItem('token');
+                        localStorage.removeItem('userId');
+                        alert('Vous etes deconnecter !');
+                    });
+                }
             }
-        }
-    });
+        });
+    }, 10)
 }
 main();
